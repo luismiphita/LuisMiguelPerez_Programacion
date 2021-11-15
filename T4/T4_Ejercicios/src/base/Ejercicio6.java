58e950427388c7f98abeb18917979ec117807219
luismiphita <luismiphita@gmail.com>
2021-11-15 11:16:28 +0100

    cambios

--- T4/T4_Ejercicios/src/base/Ejercicio6.java
+++ T4/T4_Ejercicios/src/base/Ejercicio6.java
@@ -1,23 +1,44 @@
 package base;
 
 public class Ejercicio6 {
+
+    // Escribe un programa que rellene un array de 20 números aleatorios entre el 0 y el 30.
+    // Una vez hecho esto realizará las siguientes operaciones:
+    //modificar todos los 6 por 8
+    //modificar todos los 7 por 15
+    //modificar todos los 20 por 10
+    // Al finalizar el programa deberá indicar cuántas modificaciones se han realizado.
     public static void main(String[] args) {
-        int[] numeros = new int[20];
+        int [] aletorios = new int[20];
+        int sumaModificaciones = 0;
 
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = (int) (Math.random() * 31);
+
+        // rellenar
+        for (int i = 0; i < aletorios.length; i++) {
+            //int aleatorio = (int) (Math.random() *11);
+            aletorios[i] = (int) (Math.random() *31);
         }
 
-        for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] == 6){
-                numeros[i] = 8;
-            } else if (numeros[i] == 7){
-                numeros[i] = 15;
-            } else if (numeros[i] == 20){
-                numeros[i] = 10;
+        // evaluar
+        for (int i = 0; i < aletorios.length; i++) {
+
+            if (aletorios[i] == 6){
+                aletorios[i] = 8;
+                sumaModificaciones++;
+            } else if (aletorios[i]== 7){
+                aletorios[i] = 15;
+                sumaModificaciones++;
+            } else if (aletorios[i]== 20){
+                aletorios[i] = 10;
+                sumaModificaciones++;
             }
-
-            System.out.println(numeros[i]);
         }
+
+        System.out.printf("El numero de modificaciones es: %d%n",sumaModificaciones);
+
+
     }
+
+
+
 }
