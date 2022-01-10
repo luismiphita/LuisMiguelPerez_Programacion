95d0808f0dea122939768ef9f7e4e522f3c1a43d
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2022-01-10 11:25:20 +0100

    gbdff

--- T05_poo/src/Entrada.java
+++ T05_poo/src/Entrada.java
@@ -14,6 +14,32 @@ public class Entrada {
         Usuario usuario2 = new Usuario();
         // nombre = null, pass = null, correo = null, edad = 0, fecha = null
 
+        Usuario usuario3 = new Usuario("luismi", "luismiphita@gmail.com", "pass");
+
+        Usuario usuario4 = new Usuario("luismiphita@gmail.com", "pass", 19);
+
+        // usuario1.password = "contraseña";
+        // System.out.println(usuario1.password);
+
+
+        usuario1.setPassword("contraseña");
+        System.out.println(usuario1.getPassword());
+
+        usuario1.setApellidos("Perez");
+        System.out.println(usuario1.getApellidos());
+
+        usuario1.setCorreoElectronico("luismiphita@gmail.com");
+        System.out.println(usuario1.getCorreoElectronico());
+
+        usuario1.setEdad(19);
+        System.out.println(usuario1.getEdad());
+
+        usuario1.setFecha("1 de marzo de 1743");
+        System.out.println(usuario1.getFecha());
+
+        usuario1.setNombre("Luismi");
+        System.out.println(usuario1.getNombre());
+
     }
 
 
--- T05_poo/src/Usuario.java
+++ T05_poo/src/Usuario.java
@@ -9,18 +9,78 @@ public class Usuario {
     // este constructor deja de existir cuando se escribe algun constructor
 
     public Usuario(){
-
+        this.nombre = "Por defecto";
+        this.correoElectronico = "Por defecto";
     }
     public Usuario(String nombre, String correoElectronico, String password){
-        
+
         this.nombre = nombre;
         this.correoElectronico = correoElectronico;
         this.password = password;
 
     }
 
+    public Usuario(String correoElectronico, String password, int edad){
+
+        this.correoElectronico = correoElectronico;
+        this.password = password;
+        this.edad = edad;
+
+    }
+
     // metodos
 
     // metodos especiales getters y setters
+
+    //getter de pass
+    public String getPassword(){
+        return password;
+    }
+
+    public String getCorreoElectronico(){
+        return correoElectronico;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellidos(){
+        return apellidos;
+    }
+
+    public String getFecha(){
+        return fecha;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    //setter de pass
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
 }
