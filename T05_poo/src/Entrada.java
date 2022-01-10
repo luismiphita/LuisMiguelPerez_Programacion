5c2f394cb9baa635a730eb81348424600f69a9b4
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2022-01-10 10:35:19 +0100

    update 10.01

---
+++ T05_poo/src/Entrada.java
@@ -0,0 +1,20 @@
+public class Entrada {
+
+    //ambito de clase
+    //String nombre = "Luismi";
+
+    public static void main(String[] args) {
+
+        //ambito de metodo
+        //String nombre = "Luismi";
+
+        Usuario usuario1 = new Usuario();
+        // nombre = null, pass = null, correo = null, edad = 0, fecha = null
+
+        Usuario usuario2 = new Usuario();
+        // nombre = null, pass = null, correo = null, edad = 0, fecha = null
+
+    }
+
+
+}
--- T05_poo/src/Usuario.java
+++ T05_poo/src/Usuario.java
@@ -1,2 +1,26 @@
 public class Usuario {
+
+    // variables
+    public String correoElectronico, nombre, apellidos, password, fecha;
+    public int edad;
+
+    // constructores
+    // si no hay contructores escritos, hay un constructor que es el vacio (por defecto)
+    // este constructor deja de existir cuando se escribe algun constructor
+
+    public Usuario(){
+
+    }
+    public Usuario(String nombre, String correoElectronico, String password){
+        
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.password = password;
+
+    }
+
+    // metodos
+
+    // metodos especiales getters y setters
+
 }
